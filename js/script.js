2d38537c13a8a607e24978a96b74ea833908e03f
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-01-12 10:50:44 -0300

    update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,8 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+function toggle(){
+    let toggle = document.querySelector('.popup-container')
+    toggle.classList.toggle('toggle')
+}
